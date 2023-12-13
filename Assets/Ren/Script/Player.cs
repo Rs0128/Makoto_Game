8289e47fecfbe14679933be29710972a5c0299a1
Rs0128 <Ren.sh14@icloud.com>
2023-12-13 13:36:14 +0900

    PlayerControllerとりあえず完成

--- Assets/Ren/Script/Player.cs
+++ Assets/Ren/Script/Player.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5.0f;
+    bool isMoving = false; // プレイヤーが移動中かどうかのフラグ
+    Vector3 targetPosition; // 移動の目標位置
+
+    [Tooltip("移動にかかる時間")]
+    [SerializeField] float moveTime; // 移動にかかる時間
+    [Tooltip("移動幅")]
+    [SerializeField] float movementWidth;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (!isMoving)
         {
-            StartCoroutine(SquareMoveZ());
+            if (Input.GetKeyDown(KeyCode.W))
+                StartCoroutine(MovePlayer(Vector3.forward * movementWidth));
+
+            if (Input.GetKeyDown(KeyCode.A))
+                StartCoroutine(MovePlayer(Vector3.left * movementWidth));
+
+            if (Input.GetKeyDown(KeyCode.S))
+                StartCoroutine(MovePlayer(Vector3.back * movementWidth));
+
+            if (Input.GetKeyDown(KeyCode.D))
+                StartCoroutine(MovePlayer(Vector3.right * movementWidth));
         }
     }
 
-    IEnumerator SquareMoveX()
+    IEnumerator MovePlayer(Vector3 direction)
     {
-        yield return null;
-    }
-    IEnumerator SquareMoveZ()
-    {
-        Vector3 moveDirection = new Vector3(0, 0, 1);
-        yield return null;
+        float elapsedTime = 0f;
+        isMoving = true;
+
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + direction;
+
+        while (elapsedTime < moveTime)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        transform.position = targetPosition;
+        isMoving = false;
+        
     }
 }
